b749d0501cafd09857b3d528161af718c8c4f06b
Lisa Sun <sunlisa@gmail.com>
2022-01-15 22:43:34 -0600

    aligned

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,45 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+              // SizedBox(
+              //   width: 50,
+              // ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              // SizedBox(
+              //   width: 50,
+              // ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
