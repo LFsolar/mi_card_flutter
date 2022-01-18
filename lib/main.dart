6b03ac4a73d033e5c09b57c5fbe85ec723c1d4ef
Lisa Sun <sunlisa@gmail.com>
2022-01-18 00:10:40 -0600

    add cards

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,67 @@ void main() {
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontSize: 40,
+                  fontFamily: 'Pacifico',
                 ),
-              )
+              ),
+              Text(
+                'PIKAPIKAHIKARU\'S MOM',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '123 456 7890',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'lsun1@uco.edu',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
