fdd45fa41d7b3c99bc68bbc12d8c76f9d0fbe8bf
Lisa Sun <sunlisa@gmail.com>
2022-01-18 01:01:19 -0600

    add background image

--- lib/main.dart
+++ lib/main.dart
@@ -3,83 +3,100 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/chickenramen.jpg'),
-              ),
-              Text(
-                'Lisa Sun',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  fontFamily: 'Pacifico',
+      home: Container(
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage('images/cubes.jpg'),
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/chickenramen.jpg'),
                 ),
-              ),
-              Text(
-                'PIKAPIKAHIKARU\'S MOM',
-                style: TextStyle(
-                  color: Colors.teal[100],
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  'Lisa Sun',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    fontFamily: 'Pacifico',
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                color: Colors.white,
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      '123 456 7890',
-                      style: TextStyle(
-                        color: Colors.teal[900],
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20,
+                Text(
+                  'PIKAPIKAHIKARU\'S MOM',
+                  style: TextStyle(
+                    color: Colors.teal[100],
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '012 345 6789',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                color: Colors.white,
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'lsun1@uco.edu',
-                      style: TextStyle(
-                        color: Colors.teal[900],
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'lsun1@uco.edu',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.public,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'LFsolar.github.io',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
